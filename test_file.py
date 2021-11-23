b0b32cb3ed9da52c7fe1db96eabc645cb83682b4
Tushar Sadhwani <tushar@deepsource.io>
2021-11-24 00:38:09 +0530

    fix syntax

--- test_file.py
+++ test_file.py
@@ -1,10 +1,10 @@
 from .utils import get_file_names 
 
 files = get_file_names()
-    for file in files:
-        if file.endswith('.py'):
-            python_file_found = True
-            break
-    else:
-        python_file_found = False
+for file in files:
+    if file.endswith('.py'):
+        python_file_found = True
+        break
+else:
+    python_file_found = False
 
