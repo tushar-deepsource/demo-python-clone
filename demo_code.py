bf68720f74e8e3d3406d7bbd40d2fe0cc4884f22
Tushar Sadhwani <86737547+tushar-deepsource@users.noreply.github.com>
2021-12-17 16:00:26 +0530

    Update demo_code.py

--- demo_code.py
+++ demo_code.py
@@ -1,7 +1,7 @@
-for i in range(10):
-    if x:
-        if y:
-            for j in range(i):
-                if z:
-                    if w:
-                        pass
+def f():
+    """
+    x
+    y
+    
+    z  
+    """
